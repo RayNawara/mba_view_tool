1624f41fe844fb851de64d219cd3691a88b16096
RayNawara <rjnawara@gmail.com>
2020-02-18 14:59:54 -0500

    View tool initial commit

---
+++ lib/mba_view_tool.rb
@@ -0,0 +1,7 @@
+require "mba_view_tool/version"
+require "mba_view_tool/renderer"
+
+module MbaViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/mba_view_tool/version.rb
@@ -0,0 +1,3 @@
+module MbaViewTool
+  VERSION = "0.1.0"
+end
